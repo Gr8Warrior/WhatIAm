e1e2db3a23a55a8b5bb3fd2865e98b82370c8b2a
shailandra-suriyal <shailandra.suriyal@hp.com>
2018-04-05 16:15:00 +0530

    tableview barebones added

--- WhatIAm/FeedsView/controller/FeedsViewController.swift
+++ WhatIAm/FeedsView/controller/FeedsViewController.swift
@@ -8,19 +8,45 @@
 
 import UIKit
 
-class FeedsViewController: UIViewController {
-
+class FeedsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+  
+  var tableOfFeeds: UITableView?
+  
     override func viewDidLoad() {
         super.viewDidLoad()
       self.view.backgroundColor = UIColor.blue
       edgesForExtendedLayout = UIRectEdge()
+      loadTableView()
     }
+  
+  func loadTableView() {
+    
+    tableOfFeeds = UITableView(frame: self.view.frame)
+    tableOfFeeds?.delegate = self
+    tableOfFeeds?.dataSource = self
+    self.view.addSubview(tableOfFeeds!)
+  }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 2
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    var feedCell = tableView.dequeueReusableCell(withIdentifier: "feeds")
     
+    if feedCell == nil {
+      feedCell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "feeds")
+    }
+    feedCell?.textLabel?.text = "shailu"
+    
+    return feedCell!
+  }
+  
 
     /*
     // MARK: - Navigation
